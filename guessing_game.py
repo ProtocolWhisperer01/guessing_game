595f64b05a932caf0eba2f0073ba8c03e9358a4a
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 14:52:26 +0300

    Added an exception, and a scope/range of numbers to be entered.

--- guessing_game.py
+++ guessing_game.py
@@ -10,19 +10,19 @@ sp(2) # Sleep for 2 seconds
 print("I have thought of a number 😅️!")
 
 while True:
-	
-	user_guess = input("Can you guess the number : ")
-	
-	valid_guess = int(user_guess)
-	
-	if valid_guess > x:
-		print("That number is too high!")
-		
-	elif valid_guess < x:
-		print("That number is too less!")
-		
-	else:
-		break
-
+	try:
+		user_guess = input("Can you guess the number : ")
+		valid_guess = int(user_guess)
+		if valid_guess in range(1,17):
+			if valid_guess > x:
+				print("That number is too high!")
+			elif valid_guess < x:
+				print("That number is too less!")
+			else:
+				break
+		else:
+			print("That value is out of scope!")
+	except:
+		print("Enter a valid number!")
 print("You have guessed correctly!")
 
