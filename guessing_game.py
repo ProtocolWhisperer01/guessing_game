507386c4915ce9e37abd22febce0bd0eb1e73f5a
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-04 15:13:14 +0300

    Added the attempts entry and also offered the answer at the end.

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,14 @@
+#############################################################################
+## 									   ##
+##	Title : Guessing Game						   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+# This is a simple guessing game program!
+
 import random
 from time import sleep as sp
 
@@ -5,12 +16,18 @@ print("Let's play a guessing game.\nI'm thinking of a number between 1 - 16.\n")
 
 x = random.randint(1,17)
 
-sp(2) # Sleep for 2 seconds
+sp(1.5) # Sleep for 2 seconds
 
 print("I have thought of a number 😅️!")
-
+sp(3) 
+print("Ooh! You have 4 attempts.\n")
+attempts = 0
 while True:
 	try:
+		if attempts != 4:
+			attempts += 1
+		else:
+			break
 		user_guess = input("Can you guess the number : ")
 		valid_guess = int(user_guess)
 		if valid_guess in range(1,17):
@@ -24,5 +41,8 @@ while True:
 			print("That value is out of scope!")
 	except:
 		print("Enter a valid number!")
-print("You have guessed correctly!")
 
+if valid_guess == x:
+	print("You have guessed correctly!")
+else:
+	print("Sorry you are out of guesses.\nIt was ,", x)
